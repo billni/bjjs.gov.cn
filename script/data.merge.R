b8edffc44a794f32e31e7ce8ab33f24fb4e8981a
ninidea <niyong2008@gmail.com>
2014-10-31 10:07:55 +0800

    Update data.merge.R
    修改列数引用

--- script/data.merge.R
+++ script/data.merge.R
@@ -2,7 +2,6 @@ data.merge <- function() {
 pdf.options(family='GB1')
 pdf("bjjs.gov.cn.pdf")
 path <- "csv/bjjs.gov.cn"
-n <- length(dir(path))
 all.data <- data.frame(no=1:91)
 files <- dir(path, full.names=T)
 for(filename in files) {
@@ -20,9 +19,10 @@ j<-1
 opar <- par(no.readonly=T)
 par(lty=2, pch=19, mfrow=c(2,2))
 loopnum <- nrow(nona.data)
-for(i in 1:loopnum) { 
-   plot(y=t(nona.data[i,c(3,5,7,9)]), x=1:n, type="b", col=rainbow(10), xlab=nona.data[i,2], ylab="值")
-   text(y=t(nona.data[i,c(3,5,7,9)]), x=1:n, t(nona.data[i,c(3,5,7,9)]))
+cols <- seq(from=3, to=length(all.data), by=2)
+for(i in 1:loopnum) {
+   plot(t(nona.data[i,cols]), type="b", col=rainbow(10), xlab=nona.data[i,2], ylab="值")
+   text(x=1:length(cols), y=t(nona.data[i,cols]), t(nona.data[i,cols]))
    if(i %% 10 == 1) {       
 	title(main=titles[j])
         j<-j+1
